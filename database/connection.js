35b4dec344d30f051b760c1b9e1fdb3c17d47a92
Damon Burda <damonburda96@gmail.com>
2021-03-06 21:38:51 -0800

    class to store database functions

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,7 @@
-const util = require("util");
-const mysql = require("mysql");
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
+const connection = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
@@ -13,8 +11,5 @@ const sequelize = new Sequelize(
     port: 3306,
   }  
 );
-
-connection.connect();
-connection.query = util.promisify(connection.query);
 
 module.exports = connection;
