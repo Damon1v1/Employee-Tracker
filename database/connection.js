91247efcaece477be425a871cf96a0cffd19e52e
Damon Burda <damonburda96@gmail.com>
2021-03-06 23:42:36 -0800

    inquirer prompts writing to database

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,20 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const util = require("util");
+const mysql = require("mysql");
 
-const connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }  
-);
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "",
+  database: "employees"
+});
+
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
